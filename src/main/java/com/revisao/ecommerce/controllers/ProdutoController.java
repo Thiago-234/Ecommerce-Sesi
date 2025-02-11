e31239556a7a3da60c52d81a9d3b0dc7f96f68d6
unknown <camposthiago2021@gmail.com>
2025-02-11 16:29:59 -0300

    feat: implementação da entidade produto e seus endpoints

--- src/main/java/com/revisao/ecommerce/controllers/ProdutoController.java
+++ src/main/java/com/revisao/ecommerce/controllers/ProdutoController.java
@@ -3,6 +3,8 @@ package com.revisao.ecommerce.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +20,13 @@ public class ProdutoController {
 	ProdutoService service;
 	
 
+	@GetMapping
+	public List<ProdutoDTO> findAll(){
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/pagina")
+	public Page<ProdutoDTO> findAll(Pageable pagina){
+		return service.findPagina(pagina);
+	}
 }
--- src/main/java/com/revisao/ecommerce/services/ProdutoService.java
+++ src/main/java/com/revisao/ecommerce/services/ProdutoService.java
@@ -1,18 +1,29 @@
 package com.revisao.ecommerce.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.revisao.ecommerce.dto.ProdutoDTO;
 import com.revisao.ecommerce.entities.Produto;
 import com.revisao.ecommerce.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class ProdutoService {
 
+    @Autowired
+    ProdutoRepository repo;
 
+    public List<ProdutoDTO> findAll(){
+        List<Produto> lista = repo.findAll();
+        return lista.stream().map(x-> new ProdutoDTO(x)).toList() ;
+    }
 
+    public Page<ProdutoDTO> findPagina(Pageable pagina){
+        Page<Produto> busca = repo.findAll(pagina);
+        return busca.map(x-> new ProdutoDTO(x));
+    }
+    
 }
